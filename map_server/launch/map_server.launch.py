a56283622abf3e29d9811854fc03ac24856c345d
asmbatati <asmalbatati@hotmail.com>
2025-04-22 12:16:25 +0000

    map server

---
+++ map_server/launch/map_server.launch.py
@@ -0,0 +1,63 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    # Get the directory path
+    pkg_share = get_package_share_directory('map_server')
+    rviz_config_path = os.path.join(pkg_share, 'rviz', 'map_display.rviz')
+    default_map_file = os.path.join(pkg_share, 'config', 'warehouse_map_sim.yaml')
+    
+    # Declare the map_file parameter
+    map_file_arg = DeclareLaunchArgument(
+        'map_file',
+        default_value='warehouse_map_sim.yaml',
+        description='Name of the map file to load'
+    )
+    
+    map_file = LaunchConfiguration('map_file')
+    
+    # Determine the full path to the map file
+    map_file_path = os.path.join(pkg_share, 'config', map_file)
+    
+    # Define the map_server node
+    map_server_node = Node(
+        package='nav2_map_server',
+        executable='map_server',
+        name='map_server',
+        output='screen',
+        parameters=[{'yaml_filename': map_file_path}]
+    )
+    
+    # Define the lifecycle_manager node
+    lifecycle_manager_node = Node(
+        package='nav2_lifecycle_manager',
+        executable='lifecycle_manager',
+        name='lifecycle_manager_map',
+        output='screen',
+        parameters=[
+            {'autostart': True},
+            {'node_names': ['map_server']}
+        ]
+    )
+    
+    # Define the RViz node
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_path],
+        output='screen'
+    )
+    
+    # Create and return launch description
+    return LaunchDescription([
+        map_file_arg,
+        map_server_node,
+        lifecycle_manager_node,
+        rviz_node
+    ])
---
+++ map_server/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+
+
+import os
+from glob import glob
+
+package_name = 'map_server'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # Include launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        # Include configuration files
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        # Include RViz configuration files
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='user',
+    maintainer_email='asmalbatati@hotmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
